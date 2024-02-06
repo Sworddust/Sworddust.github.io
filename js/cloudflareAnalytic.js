cf23b119227136d50ff22caf66fbf5d223d4191c
Sworddust <hmrcode@gmail.com>
2024-02-06 03:57:47 +0000

    Site updated: 2024-02-06 03:57:47

---
+++ js/cloudflareAnalytic.js
@@ -0,0 +1,4 @@
+<script
+  defer
+  src="https://static.cloudflareinsights.com/beacon.min.js?token=8523afaecd5b44bc9a111a306fec5f96&spa=false">
+  </script>
---
+++ js/googleAnalytic.js
@@ -0,0 +1,9 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KG2QJ4X4CR">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KG2QJ4X4CR');
+</script>
